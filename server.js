e0d6597e76bae94360208a10b6028375a7564b77
TheProgrammer97 <advaitsarang97@gmail.com>
2017-03-25 20:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles ={
+    'article-one:' {
+        title:'Article-one | Advait Sarang',
+        heading:'Article One',
+        date:'Mar 25,2016',
+        content:'<p>This is the content for my first article blablalblblalanlbllbalblabl</p>'
+    },
+    'article-two:'{
+        title:'Article-two | Advait Sarang',
+        heading:'Article Two',
+        date:'Mar 25,2016',
+        content:'<p>This is the content for my second article blablalblblalanlbllbalblabl</p>'
+    },
+    'article-three:'{
+        title:'Article-three | Advait Sarang',
+        heading:'Article Three',
+        date:'Mar 25,2016',
+        content:'<p>This is the content for my third article blablalblblalanlbllbalblabl</p>'
+    }
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter =0;
+app.get('/counter',function (req,res){
+    counter=counter + 1;
+    res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name',function (req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName',function (req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(article[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
